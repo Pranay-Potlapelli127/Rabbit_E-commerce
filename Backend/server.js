7a03a56e3ea52533eb94218fc8438113ec5ba537
pranay9090 <pranaykumarpotlapelli@gmail.com>
2026-08-15 22:13:32 +0530

    Admin

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,13 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const productRoutes = require("./routes/productRoutes");
+const checkoutRoutes = require("./routes/checkoutRoute");
+const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const subscribeRoutes = require("./routes/subscribeRoute");
+const adminRoutes = require("./routes/adminRoutes");
+const adminProductRoutes = require("./routes/productAdminRoutes");
+const adminOrderRoutes = require("./routes/adminOrderRoutes");
 
 dotenv.config();
 
@@ -24,6 +31,15 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/checkout", checkoutRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/", subscribeRoutes);
+
+//Admin
+app.use("/api/admin/users", adminRoutes);
+app.use("/api/admin/products", adminProductRoutes);
+app.use("/api/admin/orders", adminOrderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
